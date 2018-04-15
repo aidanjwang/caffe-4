61e0e844c29983d6b61fa279bc5358d617e75ee2
Aidan Wang <ajwang@berkeley.edu>
2018-04-15 10:10:53 -0700

    Change order details view

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from app.models import MegaOrder, MiniOrder
 from django.urls import reverse
 from django.http import HttpResponseRedirect
@@ -13,31 +13,31 @@ def create_order(request):
     if request.method == "GET":
         return render(request, 'create-order.html')
     else:
-        '''
         itemurl = request.POST['itemurl']
         # would use API
         if itemurl == "https://www.amazon.com/Cannery-Row-Centennial-John-Steinbeck/dp/014200068X/":
             name = "Cannery Row: (Centennial Edition) Paperback – Deckle Edge, February 5, 2002"
             picture = "https://images-na.ssl-images-amazon.com/images/I/51pKtoV%2Bd2L._SX332_BO1,204,203,200_.jpg"
             asin = "014200068X"
-            units = 0
+            units = 1
             price = 13.89
-        # megaOrder = MegaOrder(name=name, )
-        # megaOrder.save()
-        # return redirect("", )
+
+        megaOrder = MegaOrder(name=name, link=itemurl, picture=picture, asin=asin, units=units, price=price)
+        megaOrder.save()
+
+        return redirect("order-details", asin=asin)
         # url = reverse('app/order-details', kwargs={'name': name, 'picture': picture, 'price': price})
         # return HttpResponseRedirect(url)
         return render(request, 'order-details.html')
     # template = loader.get_template("app/order-details.html")
     # context = {'name': name; 'picture': picture; 'price': price}
     # return redirect(template.render(context, request))
-    '''
-        return HttpResponse("post")
 
 
-def order_details(request):
+def order_details(request, asin):
     if request.method == "GET":
-        return render(request, 'order-details.html')
+        mega_order = get_object_or_404(MegaOrder, asin=asin)
+        return render(request, 'order-details.html', {"mega_order": mega_order})
     else:
         order = request.POST['order']
         name = request.POST['name']
