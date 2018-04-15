5e8c075cfab368dab224295c86539a944023b703
Aidan Wang <ajwang@berkeley.edu>
2018-04-15 00:45:45 -0700

    Views

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from mysite.app.models import MegaOrder, MiniOrder
 
 
 def home(request):
@@ -10,8 +11,17 @@ def create_order(request):
     if request.method == "GET":
         return render(request, 'create-order.html')
     else:
-        return HttpResponse("Post request.") # TODO
-
+        link = request.POST['link']
+        # would use API
+        if link == "insert link":
+            name = ""
+            picture = ""
+            asin = ""
+            units = 0
+            price = 0
+        megaOrder = MegaOrder(name=name, )
+        megaOrder.save()
+        return redirect()
 
 def order_details(request):
     if request.method == "GET":
