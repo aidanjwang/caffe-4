110f3eabc061a891888b083a28b5cdf68fb082d7
Aidan Wang <ajwang@berkeley.edu>
2018-04-15 11:06:23 -0700

    Change views

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -22,8 +22,17 @@ def create_order(request):
             asin = "014200068X"
             units = 1
             price = 13.89
-            description = "Steinbeck's tough yet charming portrait of people on the margins of society, dependant on one another for both physical and emotional survival"
-        if MegaOrder.orders.filter(asin=asin).count() == 0:
+            description = "Steinbeck's tough yet charming portrait of people on the margins of society, dependant on " \
+                          "one another for both physical and emotional survival"
+        else:
+            name = "Cannery Row: (Centennial Edition) Paperback – Deckle Edge, February 5, 2002"
+            picture = "https://images-na.ssl-images-amazon.com/images/I/51pKtoV%2Bd2L._SX332_BO1,204,203,200_.jpg"
+            asin = "014200068X"
+            units = 1
+            price = 13.89
+            description = "Steinbeck's tough yet charming portrait of people on the margins of society, dependant on " \
+                          "one another for both physical and emotional survival"
+        if MegaOrder.objects.filter(asin=asin).count() == 0:
             megaOrder = MegaOrder(name=name, link=itemurl, picture=picture, asin=asin, units=units, price=price, description=description)
             megaOrder.save()
         # m = MegaOrder.orders.filter(asin=asin)
@@ -35,9 +44,7 @@ def create_order(request):
     # template = loader.get_template("app/order-details.html")
     # context = {'name': name; 'picture': picture; 'price': price}
     # return redirect(template.render(context, request))
-    '''
-        return HttpResponse("post")
-'''
+
 
 def order_details(request, asin):
     if request.method == "GET":
