b8cb6ba693f30152ef52bfd0a636b1d3caab588a
LIN Richard <richard.lin.047@berkeley.edu>
2018-04-15 11:20:44 -0700

    added valid items

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -20,18 +20,29 @@ def create_order(request):
             name = "Cannery Row: (Centennial Edition) Paperback – Deckle Edge, February 5, 2002"
             picture = "https://images-na.ssl-images-amazon.com/images/I/51pKtoV%2Bd2L._SX332_BO1,204,203,200_.jpg"
             asin = "014200068X"
-            units = 1
+            units = 10
             price = 13.89
             description = "Steinbeck's tough yet charming portrait of people on the margins of society, dependant on " \
                           "one another for both physical and emotional survival"
+        if itemurl.startswith("https://www.amazon.com/Dixon-Ticonderoga-Wood-Cased-Pencils-13872/dp/B00125Q75Y/"):
+            name = "Dixon Ticonderoga Wood-Cased #2 HB Pencils, Box of 96, Yellow (13872)"
+            picture = "https://images-na.ssl-images-amazon.com/images/I/91TSS8rv1pL._SL1500_.jpg"
+            asin = "B00125Q75Y"
+            units = 96
+            price = 9.96
+            description = "Exclusive #2 HB graphite core formula provides extra smooth performance"
+            "Includes 96 pencils comprised of 8 cases of 12 pencils each"
         else:
-            name = "Cannery Row: (Centennial Edition) Paperback – Deckle Edge, February 5, 2002"
-            picture = "https://images-na.ssl-images-amazon.com/images/I/51pKtoV%2Bd2L._SX332_BO1,204,203,200_.jpg"
-            asin = "014200068X"
-            units = 1
-            price = 13.89
-            description = "Steinbeck's tough yet charming portrait of people on the margins of society, dependant on " \
-                          "one another for both physical and emotional survival"
+            name = "Rainbow Anti-Anxiety Fidget Spinner [Metal Fidget Spinner] Figit Hand Toy for Relieving Boredom ADHD, Anxiety"
+            picture = "https://images-na.ssl-images-amazon.com/images/I/6113W0cOO%2BL._SL1001_.jpg"
+            asin = "B072LNQBZT"
+            units = 10
+            price = 9.99
+            description = "COOLEST NEW SPIN TOY – flaunt the coolest new spin toy in the market and keep calm all day long. A first"
+            "in the market, our new spin toy is designed from highly durable stainless steel with bearings that revolve at top speed" 
+            "to achieve a much longer spin. Get the most stylish new anti-anxiety fidget spinner, relieve stress or just play around,"
+            "and feel good while you’re at it."
+
         if MegaOrder.objects.filter(asin=asin).count() == 0:
             megaOrder = MegaOrder(name=name, link=itemurl, picture=picture, asin=asin, units=units, price=price, description=description)
             megaOrder.save()
